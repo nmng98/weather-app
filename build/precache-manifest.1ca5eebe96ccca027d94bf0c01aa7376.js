9dca1519a1a1ef63fef62a968055235cd3bc7d0b
Nicole Ng <nmng@ucsc.edu>
2020-10-07 01:53:46 -0700

    committing changes

---
+++ build/precache-manifest.1ca5eebe96ccca027d94bf0c01aa7376.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26193d4d4d69c0d98ed57a51bc8243cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "135a564274680dcbd90e",
+    "url": "/static/css/main.145d8158.chunk.css"
+  },
+  {
+    "revision": "736e3b53c7f5d28da6a3",
+    "url": "/static/js/2.ff51fe13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff51fe13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135a564274680dcbd90e",
+    "url": "/static/js/main.bcf009da.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "2bbd707704dcc973ba6e0b77f8845740",
+    "url": "/static/media/coldest.2bbd7077.png"
+  }
+]);
